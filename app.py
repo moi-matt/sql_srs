01585212809de73091ef0ca737e4ba1000bd831f
matthieu <matthieu.roux51@orange.fr>
2025-10-08 20:46:57 +0200

    Realistaion de ce qui etait ecrit au premier commit :)

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import duckdb as db
+
+st.write("Hello world")
+
+tab1, tab2 = st.tabs(["ma_table", "autre_table"])
+
+df = pd.DataFrame({'a': [1, 2, 3], 'b': [3, 4, 5]})
+# with tab1 :
+input_test = st.text_area(label="Entrez le texte")
+
+if input_test == "":
+    input_test = "SELECT * FROM df"
+
+st.dataframe(db.sql(input_test).df())
+
+# with tab2 :
+#     super_texte = st.text_area(label = "Entrez le texte")
+#     st.write(super_texte)
